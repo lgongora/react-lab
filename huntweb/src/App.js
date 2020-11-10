68f64c38c761b95014749252deb5f6cf72b77e57
Luiz Gongora <lgongora@hotmail.com>
2020-11-10 16:51:38 -0300

    Brincadeiras da Rocketseat

---
+++ huntweb/src/App.js
@@ -0,0 +1,14 @@
+
+import Header from './components/Header';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello Rocketseat</h1>
+    </div>
+  );
+}
+
+export default App;
